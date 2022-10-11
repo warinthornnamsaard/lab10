7e47257b9260e8df0ef9031d1d2ef36c1b01c29d
warinthornnamsaard <94583617+warinthornnamsaard@users.noreply.github.com>
2022-10-11 15:08:16 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,12 @@
+<htm>
+<body>
+<?php
+if($_COOKIE["lang"]=="en"){
+    echo "Welcome";
+}
+elseif($_COOKIE["lang"]=="th"){
+    echo "ยินดีต้อนรับ";
+    }
+?>
+</body>
+</html>
---
+++ select.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <?php
+             if(empty($_COOKIE["lang"])){
+                setcookie("lang",$_GET["language"],time()+3600*24);
+            }
+            if(isset($_COOKIE["lang"])){
+                setcookie("lang",$_GET["language"],time()+3600*24);
+            }
+        ?>
+    </body>
+</html>
